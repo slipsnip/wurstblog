89411dde5c5adbf8f8aafea54f17a0cfe382fdb0
Michael Esch <eschmichael@yandex.com>
2019-06-04 17:54:53 -0400

    fix syntax for js standard rules

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,7 @@
-var express = require('express');
-var router = express.Router();
-
+var express = require('express')
+var router = express.Router()
+const userController = require('../controllers/usersController')
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
+router.get('/', userController.index)
 
-module.exports = router;
+module.exports = router
