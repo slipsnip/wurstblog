17a7030c9d7bf7763dcbd76ceec6ab819bc5a549
Michael Esch <eschmichael@yandex.com>
2019-06-19 13:34:31 -0400

    Basic routing and view controllers

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -1,2 +1,21 @@
-exports.index = (req, res, next) =>
-  res.render('user/index', { user: 'knackwurst' })
+const path = require('path')
+const Users = require('../models/Users')
+
+exports.index = (req, res, next) => {
+  res.render('user/dashboard', {
+    title: 'Welcome', content: 'Placeholder for user landing page'
+  })
+}
+
+exports.login = (req, res, next) => {
+  res.render('user/login', {
+    title: 'Welcome - Please Login!'
+  })
+}
+
+exports.add = (req, res, next) => {
+  res.render('user/add', {
+    title: 'Add User',
+    roles: (Users.schema) ? Users.schema.path('role').enumValues : ['Admin', 'Editor', 'Author']
+  })
+}
--- routes/users.js
+++ routes/users.js
@@ -3,5 +3,9 @@ var router = express.Router()
 const userController = require('../controllers/usersController')
 /* GET users listing. */
 router.get('/', userController.index)
+router.get('/login', userController.login)
+router.post('/login', userController.login)
+router.get('/add', userController.add)
+router.post('/add', userController.add)
 
 module.exports = router
